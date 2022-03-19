de5ab8dc12fd5de33d35a9da602fb7e0c8c7ae79
Mingc <li.mingchen21@gmail.com>
2022-03-19 16:26:45 -0400

    update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import ViewBudget from './ViewBudget';
+import ChangeBudget from './ChangeBudget';
 
 
 const Budget = () => {
-    const { budget } = useContext( AppContext );
+    const { budget, dispatch } = useContext( AppContext );
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+		setIsEditing(true);
+	};
+
+	const handleSaveClick = (value) => {
+		dispatch({
+			type: 'CHANGE_BUDGET',
+			payload: value,
+		});
+		setIsEditing(false);
+	};
+
     return (
         <div className="alert alert-secondary">
-            <span>Budget: ${ budget }</span>
+            {isEditing ? (
+				<ChangeBudget handleSaveClick={handleSaveClick} budget={budget} />
+			) : (
+				// For part 1 render component inline rather than create a seperate one
+				<ViewBudget handleEditClick={handleEditClick} budget={budget} />
+			)}
         </div>
     );
 };
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const AppReducer = ( state, action ) => {
     switch ( action.type ) {
@@ -12,6 +13,11 @@ const AppReducer = ( state, action ) => {
                 ...state,
                 expenses: state.expenses.filter( ( expense ) => expense.id !== action.payload ),
             }
+        case 'CHANGE_BUDGET':
+            return {
+                ...state,
+                budget: action.payload,
+            };
         default:
             return state;
     }
@@ -20,16 +26,16 @@ const AppReducer = ( state, action ) => {
 const initialState = {
     budget: 50000,
     expenses: [
-        { id: 10, category: "shopping", name: "milk", date: new Date("2022-05-17"), cost: 40 },
-        { id: 11, category: "entertainment", name: "holiday", date: new Date("2022-01-17"), cost: 400 },
-        { id: 12, category: "transport", name: "travelling", date: new Date("2022-02-17"), cost: 40 },
-        { id: 13, category: "utility", name: "electricity", date: new Date("2022-06-17"), cost: 350 },
-        { id: 14, category: "shopping", name: "apple", date: new Date("2022-09-17"), cost: 150 },
-        { id: 15, category: "shopping", name: "wallet", date: new Date("2022-03-17"), cost: 250 },
-        { id: 16, category: "shopping", name: "ps6", date: new Date("2022-02-17"), cost: 540 },
-        { id: 17, category: "entertainmen", name: "movie", date: new Date("2022-12-17"), cost: 200 },
-        { id: 18, category: "activities", name: "party", date: new Date("2022-07-17"), cost: 500 },
-        { id: 19, category: "activities", name: "onlinecourses", date: new Date("2022-12-17"), cost: 500 },
+        { id: uuidv4(), category: "shopping", name: "milk", date: new Date("2022-05-17"), cost: 40 },
+        { id: uuidv4(), category: "entertainment", name: "holiday", date: new Date("2022-01-17"), cost: 400 },
+        { id: uuidv4(), category: "transport", name: "travelling", date: new Date("2022-02-17"), cost: 40 },
+        { id: uuidv4(), category: "utility", name: "electricity", date: new Date("2022-06-17"), cost: 350 },
+        { id: uuidv4(), category: "shopping", name: "apple", date: new Date("2022-09-17"), cost: 150 },
+        { id: uuidv4(), category: "shopping", name: "wallet", date: new Date("2022-03-17"), cost: 250 },
+        { id: uuidv4(), category: "shopping", name: "ps6", date: new Date("2022-02-17"), cost: 540 },
+        { id: uuidv4(), category: "entertainmen", name: "movie", date: new Date("2022-12-17"), cost: 200 },
+        { id: uuidv4(), category: "activities", name: "party", date: new Date("2022-07-17"), cost: 500 },
+        { id: uuidv4(), category: "activities", name: "onlinecourses", date: new Date("2022-12-17"), cost: 500 },
     ],
 };
 
